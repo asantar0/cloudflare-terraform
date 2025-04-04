a7341b0e2889e1dd69cbc5214969fd820f011658
Agustin S <agustins@root-view.com>
2025-04-04 09:50:37 -0300

    Github Actions secrets added

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "zone_id" {
+  value     = ${{ secrets.ZONE_ID }}
   type      = string
   sensitive = true
 }
 
 variable "domain" {
+  value     = ${{ secrets.DOMAIN }}
   type      = string
   sensitive = true
 }
 
 variable "api_token" {
+  value     = ${{ secrets.API_TOKEN }}
   type      = string
   sensitive = true
 }
