09b9a391e9738137d6e99569c81325b205dcc87f
Agustin S <agustins@root-view.com>
2025-04-05 22:10:59 -0300

    Deleting a record with Github Actions

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 # Add a record to the domain
-resource "cloudflare_dns_record" "www" {
+/* resource "cloudflare_dns_record" "www" {
   zone_id = var.zone_id
   comment = "Domain verification record"
   content = "200.200.200.200"
   name    = "www"
   proxied = true
   ttl     = 1
-  type    = "A"
+  type    = "A" */
 
   #ipv4_only and ipv6_only with "true" value are avaiable for special zones
-  settings = {
+/*   settings = {
     ipv4_only = false
     ipv6_only = false
-  }
+  } */
 
   #Tags are only available for paid accounts
   #tags = ["owner:SRE-team"]
